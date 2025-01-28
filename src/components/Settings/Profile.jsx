c6515eee06029f47a40b454d4c54e6d4d63f1118
cfahim880 <cfahim880@gmail.com>
2025-01-28 14:36:25 +0600

    changes done

--- src/components/Settings/Profile.jsx
+++ src/components/Settings/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
         <SettingSection icon={User} title={"Profile"}>
             <div className='flex flex-col sm:flex-row items-center mb-6'>
                 <img
-                    src="../../../public/profile-pic.png"
+                    src="https://ibb.co.com/bW8kBX8"
                     alt='Profile'
                     className='rounded-full w-20 h-20 object-cover mr-4'
                 />
